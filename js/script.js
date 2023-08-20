1ad42ebb201d7d5f7b9bc682d715824bd9e3802b
preich-work <preich_work@yahoo.com>
2023-08-20 17:50:46 -0400

    Added status button to show pet status

--- js/script.js
+++ js/script.js
@@ -29,8 +29,32 @@ const cleo = createPet("Cleo", "rat");
 const francine = createPet("Francine", "turtle");
 
 //console.log(sora, clover, baxter, cleo, francine);
-clover.sleep();
-baxter.play();
+//clover.sleep();
+//baxter.play();
 
-console.log(clover);
-console.log(baxter);
+//console.log(clover);
+//console.log(baxter);
+
+clover.isTired = 8;
+francine.isTired = 9;
+
+const allPets = [sora, clover, baxter, cleo, francine];
+
+//console.log([allPets]);
+
+const showPets = function (petArray) {
+  pets.innerHTML = "";
+  for (let pet of petArray) {
+    let status = "Ready to play!";
+    if (pet.isTired >= 7) {
+      status = "sleeping";
+    }
+
+    const li = document.createElement("li");
+    li.innerHTML = `<span class ="pet-name">${pet.name}</span> the ${pet.species} is ${status}`;
+    pets.append(li);
+  }
+};
+statusButton.addEventListener("click", function () {
+  showPets(allPets);
+});
